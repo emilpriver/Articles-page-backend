9be931b10ce7ed1fc50f00c1e12fffc2248d7929
emilpriver <emil@priver.dev>
2020-02-10 22:33:57 +0100

    Added Single Post

--- backend/merlo/posts/views.py
+++ backend/merlo/posts/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import authentication, permissions
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 
 from merlo.posts.serializers import ArticleSerializer
 from merlo.posts.models import Article
@@ -11,11 +12,22 @@ from merlo.posts.models import Article
 class ListArticles(APIView):
     """ List all articles """
 
-    def get(self, request, format=None):
+    def get(self, request, slug):
         """
-        Return a list of all users.
+        Return a list of all articles.
         """
         data = Article.objects.all()
         serialized_data = ArticleSerializer(data, many=True).data
+        return Response(serialized_data, status=status.HTTP_200_OK)
 
+
+class ListSingleArticle(APIView):
+    """ Get singel article """
+
+    def get(self, request, slug):
+        """
+        Return a list of all users.
+        """
+        data = get_object_or_404(Article, slug=slug)
+        serialized_data = ArticleSerializer(data, many=False).data
         return Response(serialized_data, status=status.HTTP_200_OK)
