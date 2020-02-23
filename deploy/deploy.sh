57b1b076864d449af27da1c717e77b3f078963d2
emilpriver <emil@priver.dev>
2020-02-23 02:48:37 +0100

    added sudo

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,8 +2,8 @@
 set -e
 
 echo "Stopping docker images..."
-docker-compose -f docker-compose.master.yml down --rmi 'all'
+sudo docker-compose -f docker-compose.master.yml down --rmi 'all'
 echo "Pulling docker images..."
-docker-compose -f docker-compose.master.yml pull
+sudo docker-compose -f docker-compose.master.yml pull
 echo "Starting docker images..."
-docker-compose -f docker-compose.master.yml up -d
+sudo docker-compose -f docker-compose.master.yml up -d
